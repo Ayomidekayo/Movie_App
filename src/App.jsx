166712ae4953239e8954baff8e6ebb9ec4bd0ee5
YourName <your-email@example.com>
2025-06-28 00:07:27 +0100

    rename file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import MobileNavigation from './components/MobileNavigation'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
